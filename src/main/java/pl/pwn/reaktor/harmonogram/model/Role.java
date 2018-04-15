79daa78b422d643113928291c45591abf86fa0f4
Malgorzata Nelson <malgorzata.nelson@gmail.com>
2018-04-15 15:41:53 +0200

    make Role entity

--- src/main/java/pl/pwn/reaktor/harmonogram/model/Role.java
+++ src/main/java/pl/pwn/reaktor/harmonogram/model/Role.java
@@ -1,6 +1,16 @@
 package pl.pwn.reaktor.harmonogram.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Role {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_role;
+
+	private String role_name;
 }
